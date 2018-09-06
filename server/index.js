ac89e85b294a17bf0266fa2b034eefb30c0a479d
Lucas <lucass.hauptmann@gmail.com>
2018-09-06 19:35:34 -0300

    Uploading project from local area to github

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// Main starting point for the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// DB Setup 
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
+
+// App Setup
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server lintening on:', port);
+
+//To run mongodb: sudo service mongod start
